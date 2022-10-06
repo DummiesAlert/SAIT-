422b78568a7babeb80ea458e17d4d37c3bb62014
DummiesAlert <100309969+DummiesAlert@users.noreply.github.com>
2022-10-06 13:50:57 -0600

    SAIT

---
+++ GitHub/SAIT-/CPRG216S/Lab_Syntax_Rules_Conditions_And_Loops/3_Area_Of_Trapezoid.py
@@ -0,0 +1,7 @@
+LengthA = float(input("Enter Length A:"))
+LengthB = float(input("Enter Length B:"))
+Height = float(input("Enter Height:"))
+
+Area = ((LengthA+LengthB)/2) * Height 
+
+print ("The Area of the Trapezoid is: ", Area)
---
+++ GitHub/SAIT-/CPRG216S/Lab_Syntax_Rules_Conditions_And_Loops/9_Formatted_Print_To_Screen.py
@@ -0,0 +1,9 @@
+pi = 3.14159
+
+x = pi
+y = 2
+
+z1 = x * y
+z2 = round(z1,3)
+
+print ("Multiplying", x, "and", y, "gives", z2)
