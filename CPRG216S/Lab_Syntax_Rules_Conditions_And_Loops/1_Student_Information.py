ed3af169f530a6c71aba9190a6665d63b6cfb3ec
DummiesAlert <100309969+DummiesAlert@users.noreply.github.com>
2022-10-06 13:25:19 -0600

    SAIT

---
+++ CPRG216S/Lab_Syntax_Rules_Conditions_And_Loops/1_Student_Information.py
@@ -0,0 +1,11 @@
+print("Welcome")
+
+FName = str(input("Enter First Name:"))
+LName = str(input("Enter Last Name:"))
+Age = int(input("Enter Age:"))
+GPA = float(input("Enter GPA:"))
+Major = str(input("Enter Major:"))
+Address = str(input("Enter Address:"))
+
+print("Name:" , FName , LName, "Age:", Age, "GPA:", GPA, "Major:", Major, "Address: ", Address)
+
---
+++ CPRG216S/Lab_Syntax_Rules_Conditions_And_Loops/8_Average_Of_Integers.py
@@ -0,0 +1,5 @@
+Sum = 1 + 2 + 3 + 4 + 5
+
+Average= Sum/5.0
+
+print("Average: ", Average)
